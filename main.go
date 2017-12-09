74b361a11f1cfa983cfcfabd1500a880506d6c0d
Gaige Pierce-Raison <gaige@chatsubo.net>
2017-12-08 23:25:52 -0800

    Added checking for validation that the supplied command exists.

--- main.go
+++ main.go
@@ -2,22 +2,49 @@ package main
 
 import "flag"
 import "log"
-import "os"
+import "os/exec"
 
 import "github.com/fsnotify/fsnotify"
+
+type Options struct {
+	Recursive bool
+	Command   string
+	Paths     []string
+}
 
 func main() {
 	var target = flag.String("target", ".", "Specifiy a path to a file or directory to watch.")
 	var recursive = flag.Bool("recursive", false, "Watch all subdirectories, if target is a directory.")
 	var command = flag.String("command", "", "Command to execute as an event callback.")
 	flag.Parse()
-	log.Println(os.Args, *target, *recursive, *command)
+
+	cmd, err := exec.LookPath(*command)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	opts := Options{
+		Recursive: *recursive,
+		Command:   cmd,
+		Paths:     nil,
+	}
+
+	log.Println(opts)
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer watcher.Close()
+
+	// TODO: Make recursive!
+	if *recursive {
+	} else {
+		err = watcher.Add(*target)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	done := make(chan bool)
 	go func() {
@@ -41,17 +68,6 @@ func main() {
 			}
 		}
 	}()
-
-	// TODO: Make recursive!
-	if *recursive {
-
-	} else {
-		err = watcher.Add(*target)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
 	// TODO: Make signals get handled and that is how to kill the process
 	<-done
 }
