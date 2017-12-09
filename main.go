c4c582f13a1d4a729d1f76f321740780e25e18b2
Gaige Pierce-Raison <gaige@chatsubo.net>
2017-12-08 22:23:38 -0800

    Added a simple event handler for files being watched.

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
 import "flag"
-import "fmt"
+import "log"
 import "os"
 
-//import "github.com/fsnotify/fsnotify"
+import "github.com/fsnotify/fsnotify"
 
 func main() {
-	var watchTarget = flag.String("target", ".", "Specifiy a path to a file or directory to watch.")
+	var target = flag.String("target", ".", "Specifiy a path to a file or directory to watch.")
 	var recursive = flag.Bool("recursive", false, "Watch all subdirectories, if target is a directory.")
 	var command = flag.String("command", "", "Command to execute as an event callback.")
 	flag.Parse()
+	log.Println(os.Args, *target, *recursive, *command)
 
-	fmt.Println(os.Args, *watchTarget, *recursive, *command)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				switch {
+				case event.Op&fsnotify.Write == fsnotify.Write:
+					log.Println("WRITE:", event.Name)
+				case event.Op&fsnotify.Create == fsnotify.Create:
+					log.Println("CREAT:", event.Name)
+				case event.Op&fsnotify.Remove == fsnotify.Remove:
+					log.Println("REMOV:", event.Name)
+				case event.Op&fsnotify.Rename == fsnotify.Rename:
+					log.Println("RENAM:", event.Name)
+				case event.Op&fsnotify.Chmod == fsnotify.Chmod:
+					log.Println("CHMOD:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("ERROR:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(*target)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 }
